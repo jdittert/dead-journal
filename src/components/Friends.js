e705d0a2e04bacc15fcaf888e339c8ce4d9285be
jdittert <jondittert@gmail.com>
2023-06-27 21:20:57 -0400

    Populate friends page with friends' entries

--- src/components/Friends.js
+++ src/components/Friends.js
@@ -1,8 +1,55 @@
-import React from 'react';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect, useState } from 'react';
+// import { useAuth } from '../contexts/AuthContext';
+import { useInfo } from '../contexts/InfoContext';
+import { db } from '../firebase';
+import { collection, query, where, getDocs, orderBy} from 'firebase/firestore';
+import Entry from './Entry';
 
 export default function Friends() {
+    // const { currentUser } = useAuth()
+    const { userInfo } = useInfo()
+    const [error, setError] = useState('')
+    const [entries, setEntries] = useState([])
+    const entriesRef = collection(db, 'entries')
+
+    const follows = userInfo.follows
+    const q = query(entriesRef, where('user', 'in', follows), orderBy('timestamp', 'desc'))   
+    
+    useEffect(() => {
+        setError('')
+        console.log(follows)
+        async function getEntries() {
+            const querySnapshot = await getDocs(q)
+            const tempEntries = []
+            querySnapshot.forEach((doc) => {
+                tempEntries.push({...doc.data(), id: doc.id})
+            })
+            setEntries(tempEntries)
+            console.log(entries)
+        }
+
+        return () => getEntries() 
+
+    }, [])
+
+    if(!entries) setError('Entries failed to load.')
 
     return (
-        <div className='main-wrapper'>Your friends' entries:</div>
+        <div className='main-wrapper'>
+            {error && <div className='error-message'>{error}</div>}
+            <div className='small-title'>{userInfo.username}'s Friends' Entries:</div>
+            {entries.length === 0 ? 
+            <div>This user has no public entries.</div> : 
+            entries.map((entry) => {
+                return (
+                    <>
+                        <Entry entry={entry} />
+                    </>
+                )
+            })
+        }
+            
+        </div>
     )
 }
